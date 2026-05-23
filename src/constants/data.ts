9c4ad65390153c976915af50aa5b9f6f0cc9248c
Loucif Tameur Ahmed <tameur_ahmed.loucif@univ-constantine2.dz>
2026-05-23 23:58:30 +0100

    add sewing project

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -4,10 +4,10 @@ import noteapp from "../assets/image.webp";
 import headphone from "../assets/Screenshot 2026-03-15 151330.webp";
 import weather from "../assets/Screenshot 2026-04-08 194802.webp";
 import oop from "../assets/Screenshot 2026-04-08 200005.webp";
-import profd from "../assets/Screenshot 2026-04-08 195815.webp";
 import estinhackathon from "../assets/Screenshot 2026-04-20 114159.png";
 import rihlaweb from "../assets/image.png";
 import aiweb from "../assets/download (24).jpg";
+import sewing from "../assets/Screenshot 2026-05-23 235542.png";
 import {
   JavascriptIcon,
   TypescriptIcon,
@@ -139,6 +139,23 @@ export const projects: Array<{
   },
   {
     id: 2,
+    title: "Maison de Couture - أم وائل",
+    description:
+      "A premium tailoring portfolio platform for a professional Algerian home-based fashion designer, featuring elegant animations, luxury UI, and an interactive showcase of traditional and modern designs.",
+    image: sewing,
+    tech: [
+      { label: "React", icon: ReactIcon },
+      { label: "TypeScript", icon: TypescriptIcon },
+      { label: "Vite", icon: ViteIcon },
+      { label: "Tailwind", icon: TaillwindIcon },
+    ],
+    hackathon: "",
+    liveDemo: "",
+    github: "",
+    figma: "",
+  },
+  {
+    id: 3,
     title: "Note Web App",
     description:
       "Full-stack note-taking application with authentication and CRUD features using REST APIs.",
@@ -159,7 +176,7 @@ export const projects: Array<{
       "https://www.figma.com/design/rm2Fvk9U1lDoC3paoBDvge/note-app?node-id=0-1&t=JMrUlY7hLogXADuu-1",
   },
   {
-    id: 3,
+    id: 4,
     title: "Headphone Website",
     description:
       "Modern product landing page with clean UI/UX design and a fully responsive layout.",
@@ -177,7 +194,7 @@ export const projects: Array<{
       "https://www.figma.com/design/8CyxT9T0MwuaRxzPNhJDl3/headbods?node-id=0-1&t=hLAwadaGzomyeBGf-1",
   },
   {
-    id: 4,
+    id: 5,
     title: "Weather App",
     description:
       "Weather forecasting app using external APIs with real-time data and an interactive UI.",
@@ -196,7 +213,7 @@ export const projects: Array<{
       "https://www.figma.com/design/wXIbw0NNKQHh5LXUW9SMsg/weather-web?node-id=0-1&t=onOswin6pBI3t0Tu-1",
   },
   {
-    id: 5,
+    id: 6,
     title: "library book Hero Section",
     description:
       "Web application for browsing and managing a digital library with a responsive and user-friendly UI.",
@@ -214,7 +231,7 @@ export const projects: Array<{
       "https://www.figma.com/design/jqsnmcwi1RK0rZuzMR3ESr/book-library?node-id=0-1&t=JBJeWvcJ6pyxEyGx-1",
   },
   {
-    id: 6,
+    id: 7,
     title: "Rihla Web",
     description:
       "A full-stack web application that helps users plan and manage trips by organizing destinations, travel details, and itineraries with a clean, responsive UI.",
@@ -232,7 +249,7 @@ export const projects: Array<{
     figma: "https://www.figma.com/deck/F8gNPiNluy6D8r2CgLmZSQ",
   },
   {
-    id: 7,
+    id: 8,
     title: "Teck web store",
     description: "Responsive web app built by ai for student project ",
     image: aiweb,
@@ -247,7 +264,7 @@ export const projects: Array<{
     figma: "",
   },
   {
-    id: 8,
+    id: 9,
     title: "Network Simulation",
     description:
       "Network simulation system with visualization, implementing core networking algorithms",
@@ -260,24 +277,6 @@ export const projects: Array<{
     hackathon: "",
     liveDemo: "",
     github: "https://github.com/lo-ucif/oop-p2.git",
-    figma: "",
-  },
-  {
-    id: 9,
-    title: "Prof Manager Dashboard",
-    description:
-      "A professional profile page for department managers with a clean and structured UI, designed and built to present key information efficiently.",
-    image: profd,
-    tech: [
-      { label: "HTML", icon: HtmlIcon },
-      { label: "CSS", icon: CssIcon },
-      { label: "JavaScript", icon: JavascriptIcon },
-      { label: "Java", icon: JavaIcon },
-      { label: "Mysql", icon: MysqlIcon },
-    ],
-    hackathon: "",
-    liveDemo: "",
-    github: "",
     figma: "",
   },
 ];
